2b666ca3369e47c9e29a6fb90f5814c3172003bc
George Olufemi <93535171+George-Olufemi@users.noreply.github.com>
2022-06-11 22:35:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   let interval = useRef();
 
   const startTimer = () => {
-    const countdownDate = new Date('June 12 2022 00:00:00').getTime();
+    const countdownDate = new Date('June 15 2022 00:00:00').getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
