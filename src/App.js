684e133a1914362f10dd045796b59f3a3eed297f
George Olufemi <93535171+George-Olufemi@users.noreply.github.com>
2022-06-11 22:43:03 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ const App = () => {
             <span className=''><svg xmlns="http://www.w3.org/2000/svg" className="pic h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
             </svg></span>
-            <h2>Countdown To Next service</h2>
+            <h2>Countdown To Next Service</h2>
             <p>Countdown to Our Services!. One you should never miss.</p>
           </div>
 
