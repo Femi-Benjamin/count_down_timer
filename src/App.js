92b9d18ff0caa79a71815e76212bb0f5e2ab81cd
Femi-Benjamin <benjaminolufemi16@gmail.com>
2022-06-11 22:24:49 -0700

    count_down_timer now working

--- src/App.js
+++ src/App.js
@@ -11,29 +11,29 @@ const App = () => {
 
   const startTimer = () => {
     const countdownDate = new Date('June 30 2022 00:00:00').getTime();
+
+    interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+      
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000*60*60)));
+      const minutes = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60 ));
+      const seconds = Math.floor(((distance % (1000 * 60 ))) / 1000);
+  
+      if (distance < 0 ){
+        // stop timer   
+        clearInterval(interval.current);
+      } else {
+       // update timer
+       setTimerDays(days);
+       setTimerHours(hours);
+       setTimerMinutes(minutes);
+       setTimerSeconds(seconds);
+      }
+    }, 1000);
+  
   };
-
-  interval = setInterval(() => {
-    const now = new Date().getTime();
-    const distance =  - now;
-    
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000*60*60)));
-    const minutes = Math.floor((distance % (1000 * 60 * 60 )) / (1000 * 60 ));
-    const seconds = Math.floor(((distance % (1000 * 60 ))) / 1000);
-
-    if (distance < 0 ){
-      // stop timer   
-      clearInterval(interval.current);
-    } else {
-     // update timer
-    setTimerDays(days);
-    setTimerHours(hours);
-    setTimerMinutes(minutes);
-    setTimerSeconds(seconds);
-  }
-
-  }, 1000);
 
 
   //componentDidMount
